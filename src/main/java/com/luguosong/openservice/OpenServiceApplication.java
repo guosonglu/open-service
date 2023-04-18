1272bb67c9509bd9775ced007431cee699d1571e
luguosong <1054595718@qq.com>
2023-04-18 16:35:50 +0800

    Initial commit

---
+++ src/main/java/com/luguosong/openservice/OpenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luguosong.openservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/luguosong/openservice/controller/itext7/Itext01DigitalSignatures.java
@@ -0,0 +1,5 @@
+package com.luguosong.openservice.controller.itext7;/**
+ * 
+ * @author luguosong
+ */public class Itext01DigitalSignatures {
+}
